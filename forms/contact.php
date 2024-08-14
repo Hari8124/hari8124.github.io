972d460a6dc14d0fa43df2d25c887bbb697ba6c7
Hari Nath <142809042+Hari8124@users.noreply.github.com>
2024-08-14 12:34:46 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -6,7 +6,7 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  $receiving_email_address = 'harinath8124@gmail.com.com';
+  $receiving_email_address = 'harinath8124@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
